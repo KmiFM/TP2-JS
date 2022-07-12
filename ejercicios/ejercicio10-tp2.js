254bd79cce323b9071f7793e3c0876f2bd2575ce
Cami <camilafuentesmaldonado@gmail.com>
2022-07-12 19:05:11 -0300

    hicimos punto 10 en clase

---
+++ ejercicios/ejercicio10-tp2.js
@@ -0,0 +1,19 @@
+//pedir al usuario filas y un nunmero de columnas
+//dibujar una tabla
+//cada celda debe tener un numero descendente de multiplicar filas por columnas
+const  filas=parseInt(prompt('ingrese el numero de filas'));
+const columnas=parseInt(prompt('ingrese un numero de columnas'));
+let resultado = filas*columnas;
+document.write(`<table> <tbody>`);
+
+// ......
+
+for(let indiceFila=0;indiceFila<filas; indiceFila++){
+    document.write(`<tr>`)
+    for(let indiceColumna=0;indiceColumna<columnas;indiceColumna++){
+    document.write(`<td>${resultado}</td>`)
+    resultado--;
+    }      
+    document.write(`</tr>`)
+    }
+document.write(`</tbody> </table>`)
