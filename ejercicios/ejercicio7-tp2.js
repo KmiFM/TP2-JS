189d902d20819d10df58e4f32ddddd4d953a67b3
Cami <camilafuentesmaldonado@gmail.com>
2022-07-13 16:37:40 -0300

    realicé el punto 7

---
+++ ejercicios/ejercicio7-tp2.js
@@ -0,0 +1,28 @@
+/*7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
+
+303030303030303030303030303030303030303030303030303030303030
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1
+*/
+var numero = parseInt(prompt('escriba su número'));
+if(Number(numero) == numero ){
+    if(numero > 0 && numero <=50){
+     var rep, i ;
+     for(i=numero; i >=1; i--){
+        for(rep=i;rep>=1;rep--){
+            document.write(i);
+   }
+   document.write('<br>');
+  }
+ }
+ else{
+    alert('El valor introducido es mayor a 50, no esta dentro del rango o no es un valor válido');
+ }
+}
+else{
+    alert('ese no es un valor numerico!');
+}
