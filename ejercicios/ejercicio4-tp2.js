ad02033d05be174a77d4caa18959426db8431611
Cami <camilafuentesmaldonado@gmail.com>
2022-07-14 12:58:11 -0300

    realicé el putno 4

--- ejercicios/ejercicio4-tp2.js
+++ ejercicios/ejercicio4-tp2.js
@@ -1,17 +1,26 @@
 /* 4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
 */
-let suma = 0;
 
-if {
+var suma = 0;
+
+do {
     
-    let numero = parseInt(prompt("Introduce un numero"));
+    var numero = prompt("Introduce un numero");
     
     if (Number(numero) == numero) {
         
+        numero = Number(numero);
+        
         suma = suma + numero;
     }
+    
+    else {
+    
+        if (numero != undefined) {
+            alert(numero + " No es un numero");
+        }
+    }
 
-} else{
+} while (numero != undefined);
 
 document.write(suma);
-}
